5db0b5b382cbe38d8803e841dfbb4afb76c8c6db
Abdur Rahaman <54670007+AbdurRahamanWP@users.noreply.github.com>
2025-01-25 14:33:50 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,37 +1,59 @@
-jQuery(document).ready(function () {
- 
- // Handle image upload using wp.media library
- jQuery('#button_icon_img').on('click', function () {
-   
-    var images = wp.media({
-        title: 'Upload Image',
-        multiple: true
-    }).open().on("select", function () {
+(function( $ ) {
+	'use strict';
 
-        // Get the selected image from the media library
-        var uploadImage = images.state().get('selection').first();
-        var selectedImage = uploadImage.toJSON().url;
-
-        // Display the selected image in an HTML element with id="showImage"
-        jQuery('#show_image').html("<img src='" + selectedImage + "'/>");
-        jQuery('#button_image').val(selectedImage);
-    });
-});
-
-
-jQuery('#scroll_top_form').on('submit', function (e) {
-  e.preventDefault();
-  // alert('form submit');
-
-  $.post(
-    ajaxScrollTop.ajax_url,
-    $(this).serialize(),
-    function(response){}
+    jQuery(document).ready(function () {
     
-  );
+    // Handle image upload using wp.media library
+    jQuery('#button_icon_img').on('click', function () {
+      
+        var images = wp.media({
+            title: 'Upload Image',
+            multiple: true
+        }).open().on("select", function () {
 
-});
+            // Get the selected image from the media library
+            var uploadImage = images.state().get('selection').first();
+            var selectedImage = uploadImage.toJSON().url;
+
+            // Display the selected image in an HTML element with id="showImage"
+            jQuery('#show_image').html("<img src='" + selectedImage + "'/>");
+            jQuery('#button_image').val(selectedImage);
+        });
+    });
 
 
 
-});
+
+    });
+
+    jQuery(document).ready(function ($) {
+      $('#scroll_top_form').on('submit', function (e) {
+          e.preventDefault();
+  
+   
+          const formData = $(this).serializeArray();
+          const data = {
+              action: 'handle_scroll_top',
+              scroll_top_nonce: $('#scroll_top_nonce').val(),
+          };
+  
+          formData.forEach((item) => {
+              data[item.name] = item.value;
+          });
+  
+          // Use $.post() for AJAX request
+          $.post(ajaxScrollTop.ajax_url, data, function (response) {
+              if (response.success) {
+                  alert(response.data); 
+              } else {
+                  alert(response.data); 
+              }
+          }).fail(function () {
+              alert('An error occurred while processing the request.');
+          });
+      });
+  });
+  
+  
+
+})( jQuery );
--- web-scroll-to-top.php
+++ web-scroll-to-top.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Ajax Scroll To Top
  * Plugin URL: https://github.com/AbdurRahamanWP/
@@ -16,89 +17,122 @@
 
 
 // If this file is called directly, abort.
-if ( ! defined( 'ABSPATH' ) ) {
-	die;
+if (! defined('ABSPATH')) {
+  die;
 }
 
 /** 
  * include script CSS/Js
  */
 
- add_action('wp_enqueue_scripts','Web_Scroll_Top_form_enqueue_styles');
- add_action('admin_enqueue_scripts','Web_Scroll_Top_form_enqueue_admin_styles');
+add_action('wp_enqueue_scripts', 'Web_Scroll_Top_form_enqueue_styles');
+add_action('admin_enqueue_scripts', 'Web_Scroll_Top_form_enqueue_admin_styles');
 
 
- add_action('wp_ajax_scroll_top_form','updated_scroll_to_top_data');
+// add_action('wp_ajax_scroll_top_form', 'updated_scroll_to_top_data');
+
+add_action('wp_ajax_handle_scroll_top', 'handle_scroll_top_callback');
+add_action('wp_ajax_nopriv_handle_scroll_top', 'handle_scroll_top_callback');
+
+function handle_scroll_top_callback()
+{
+  // Verify nonce
+  if (!isset($_POST['scroll_top_nonce']) || !wp_verify_nonce($_POST['scroll_top_nonce'], 'simple_ajax_scroll_top')) {
+    wp_send_json_error('Invalid nonce.');
+    wp_die();
+  }
+
+  // Sanitize and prepare data
+  $data = [
+    'scroll_top_type'  => sanitize_text_field($_POST['scroll_top_type']),
+    'button_height'    => sanitize_text_field($_POST['button_height']),
+    'button_width'     => sanitize_text_field($_POST['button_width']),
+    'padding_right'    => sanitize_text_field($_POST['padding_right']),
+    'padding_buttom'   => sanitize_text_field($_POST['padding_buttom']),
+    'padding_all'      => sanitize_text_field($_POST['padding_all']),
+    'border_Radius'    => sanitize_text_field($_POST['border_Radius']),
+    'background_color' => sanitize_hex_color($_POST['background_color']),
+    //'button_image'     => esc_url_raw($_POST['button_image']),
+    'text_color'       => sanitize_hex_color($_POST['text_color']),
+    'font_size'        => sanitize_text_field($_POST['font_size']),
+  ];
+
+  // Save data to options
+  update_option('scroll_to_top', wp_json_encode($data));
+
+  // Return success response
+  wp_send_json_success('Form data saved successfully.');
+  wp_die();
+}
 
 
- function Web_Scroll_Top_form_enqueue_styles(){
+
+function Web_Scroll_Top_form_enqueue_styles()
+{
 
 
-  $settings = json_decode(get_option('scroll_to_top'),true);
+  $settings = json_decode(get_option('scroll_to_top'), true);
   @$scroll_top_type = $settings['scroll_top_type'];
 
-      if($scroll_top_type =='tab'){
-        require_once plugin_dir_path( __FILE__ ) . 'includes/Admin/tab_style.php';
-      }
-      if($scroll_top_type =='images'){
-        require_once plugin_dir_path( __FILE__ ) . 'includes/Admin/images_style.php';
-      }
-      if($scroll_top_type =='link'){
-        require_once plugin_dir_path( __FILE__ ) . 'includes/Admin/link_style.php';
-      }
-      if($scroll_top_type =='pill'){
-        require_once plugin_dir_path( __FILE__ ) . 'includes/Admin/pill_style.php';
-      }
-  
+  if ($scroll_top_type == 'tab') {
+    require_once plugin_dir_path(__FILE__) . 'includes/Admin/tab_style.php';
+  }
+  if ($scroll_top_type == 'images') {
+    require_once plugin_dir_path(__FILE__) . 'includes/Admin/images_style.php';
+  }
+  if ($scroll_top_type == 'link') {
+    require_once plugin_dir_path(__FILE__) . 'includes/Admin/link_style.php';
+  }
+  if ($scroll_top_type == 'pill') {
+    require_once plugin_dir_path(__FILE__) . 'includes/Admin/pill_style.php';
+  }
 }
 
 
-function updated_scroll_to_top_data(){
+function updated_scroll_to_top_data()
+{
 
   wp_send_json_success($_POST);
-
 }
 
 
 
-function Web_Scroll_Top_form_enqueue_admin_styles(){
-  wp_enqueue_style( 'bootstrap', plugins_url('css/bootstrap.css',__FILE__));
-  wp_enqueue_style( 'bootstrap-min', plugins_url('css/bootstrap.min.css',__FILE__));
-  wp_enqueue_style( 'admins', plugins_url('css/admin.css',__FILE__));
-  wp_enqueue_script( 'web_scroll_top_custom_js', plugins_url('js/custom.js',__FILE__));
-  
-  wp_localize_script('web_scroll_top_custom_js', 'ajaxScrollTop',[
-      'ajax_url' => admin_url('admin-ajax.php'),
-      'nonce'    => wp_create_nonce('simple_ajax_scroll_top'),
+function Web_Scroll_Top_form_enqueue_admin_styles()
+{
+  wp_enqueue_style('bootstrap', plugins_url('css/bootstrap.css', __FILE__));
+  wp_enqueue_style('bootstrap-min', plugins_url('css/bootstrap.min.css', __FILE__));
+  wp_enqueue_style('admins', plugins_url('css/admin.css', __FILE__));
+  wp_enqueue_script('web_scroll_top_custom_js', plugins_url('js/custom.js', __FILE__));
+
+  wp_localize_script('web_scroll_top_custom_js', 'ajaxScrollTop', [
+    'ajax_url' => admin_url('admin-ajax.php'),
+    'nonce'    => wp_create_nonce('simple_ajax_scroll_top'),
   ]);
-  
+
   wp_enqueue_media();
 }
 
 
 
-add_action('wp_enqueue_scripts','Web_Scroll_Top_form_enqueue_script');
-function Web_Scroll_Top_form_enqueue_script(){
-    wp_enqueue_script('jquery');
-    wp_enqueue_script( 'web_scrollUp', plugins_url('js/web_scrollUp.js',__FILE__));
-    
+add_action('wp_enqueue_scripts', 'Web_Scroll_Top_form_enqueue_script');
+function Web_Scroll_Top_form_enqueue_script()
+{
+  wp_enqueue_script('jquery');
+  wp_enqueue_script('web_scrollUp', plugins_url('js/web_scrollUp.js', __FILE__));
 }
 
-function web_scroll_top_function(){
+function web_scroll_top_function()
+{
 ?>
-<script>
-
-    jQuery(document).ready(function(){
-        jQuery.scrollUp();
+  <script>
+    jQuery(document).ready(function() {
+      jQuery.scrollUp();
     });
+  </script>
 
-</script>
-
-<?php 
+<?php
 }
-add_action('wp_footer','web_scroll_top_function');
+add_action('wp_footer', 'web_scroll_top_function');
 
 // Add Menu Options
-require_once plugin_dir_path( __FILE__ ) . 'includes/Admin/menu.php';
-
-
+require_once plugin_dir_path(__FILE__) . 'includes/Admin/menu.php';
